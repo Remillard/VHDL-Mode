0de8fa9c661f905462256c5fa9322b8511e66af4
Mark Norton <remillard@gmail.com>
2018-07-13 08:51:25 -0500

    Issue #92 and documentation for release
    Added release notes for Sven's changes and was able to quickly add in a fix for the constant expressions.

--- vhdl_mode.py
+++ vhdl_mode.py
@@ -20,7 +20,7 @@ class vhdlModeVersionCommand(sublime_plugin.TextCommand):
     Prints the version to the console.
     """
     def run(self, edit):
-        print("vhdl-mode: VHDL Mode Version 1.7.12")
+        print("vhdl-mode: VHDL Mode Version 1.7.13")
 
 #----------------------------------------------------------------
 class vhdlModeInsertHeaderCommand(sublime_plugin.TextCommand):
